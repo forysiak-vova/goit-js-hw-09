3413d6fbada507d9aef154ceee086867eae7ab69
forysiak-vova <volodja.forus@gmail.com>
2021-11-14 10:30:21 +0100

    e

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const startbtn = document.querySelector('[data-start]');
+const stoptbtn = document.querySelector('[data-stop]');
+const bodyCol = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+
+
+startbtn.addEventListener('click', onClickBtnStart);
+stoptbtn.addEventListener('click', onClickBtnStop);
+
+let timeoutId = null;
+const NOTIFICATION_DELAY = 1000;
+
+function onClickBtnStart() {
+   const color = `${getRandomHexColor()}`;
+   bodyCol.style.backgroundColor = color;
+ 
+//   timeoutId = setTimeout(onClickBtnStart, NOTIFICATION_DELAY);
+   timeoutId = setTimeout(() => {
+      onClickBtnStart()
+      console.log(timeoutId);
+     
+   },NOTIFICATION_DELAY);
+};
+ 
+function onClickBtnStop() {
+
+    clearTimeout(timeoutId);
+};
+
+
+
+
 
